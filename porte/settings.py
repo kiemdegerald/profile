e83d019631c42185f106b71780e1281a2cd4c14d
kiemdegerald <kiemdegeraldo9@gmail.com>
2025-09-14 18:46:16 +0000

    Fix: Ajouter geralddev.onrender.com à ALLOWED_HOSTS

--- porte/settings.py
+++ porte/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-r#ss7jq%)vcd+^uo^q$&)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,geralddev.onrender.com').split(',')
 
 
 # Application definition
